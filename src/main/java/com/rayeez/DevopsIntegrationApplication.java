7546225f6dadc18d762e37cc7b91f45ffca8bd26
rayeez <rayeez@mail.com>
2022-07-18 13:26:39 -0700

    Updated Welcome message.

--- src/main/java/com/rayeez/DevopsIntegrationApplication.java
+++ src/main/java/com/rayeez/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "welcome to javatechie";
+		return "Hello World!";
 	}
 
 	public static void main(String[] args) {
